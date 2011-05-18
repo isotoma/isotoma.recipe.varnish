51e739fd56c36064a2dbc12f9d9d91719221ff47
Chris Hannam <bassdread@gmail.com>
2011-05-18 09:17:21 +0100

    Added fake bootstrap to make CI configuration more generic

---
+++ bootstrap.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python 
+
+"""A holder file as we don't need a bootstrap but this CI configs to be more generic."""
+
+def main():
+    """Do nothing as the bootstrap isn't required."""
+    pass
+
+if __name__ == "__main__":
+    main()
+
